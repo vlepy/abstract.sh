e07094461f709654906ace169bf8fa0c7eac6836
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-04 22:24:00 +0100

    corrections

--- abstract.sh
+++ abstract.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#specify your pictures directory
 picdir=$HOME/Obrazy
+
 cd $picdir
 while [ 1 ]; do
 	var=`curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1 | awk -F "/" '{print $(NF)}'`
