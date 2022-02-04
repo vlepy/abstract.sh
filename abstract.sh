43825beff07b63bb76f61f7c4e9897fdb91cecec
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-04 23:31:32 +0100

    corrections

--- abstract.sh
+++ abstract.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #specify your pictures directory
 picdir=$HOME/Pictures
+#specify your environment
+de="ubuntu"
+#de=xfce
+#de=plasma
 
 cd $picdir
 while [ 1 ]; do
@@ -9,8 +13,14 @@ while [ 1 ]; do
 		echo "No new abstracts."
 	else
 		wget `curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1`
-	gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+		if [ $de == "ubuntu" ] || [ $de == "gnome" ]; then
+			gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+		elif [ $de == "plasma" ]; then
+			qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///media/sda2/Background/SpaceWall/Escape_Function.jpg")}'
+		elif
+			[ $de == "xfce" ]; then
+			xfconf-query -c xfce4-desktop -p  /backdrop/screen0/monitor0/workspace0/last-image -s $picdir/$var
+		fi
 	fi
 sleep 1h
 done
-
