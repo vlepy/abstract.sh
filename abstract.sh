a607ce2785eacc5183a63977083a5f796d49a00e
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-05 04:08:41 +0100

    corrections

--- abstract.sh
+++ abstract.sh
@@ -7,8 +7,8 @@ picdir=$HOME/Pictures
 #specify your environment
 #de="ubuntu"
 #de="gnome"
-#de="xfce"
-de="plasma"
+de="xfce"
+#de="plasma"
 #de="mate"
 #de="lxde"
 #-----------------
@@ -17,23 +17,28 @@ de="plasma"
 cd $picdir
 while [ 1 ]; do
 	var=`curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1 | awk -F "/" '{print $(NF)}'`
-	if [ -f $picdir/$var ]; then
-		echo "No new abstracts."
-	else
-		curl -o $var `curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1`
-		if [ $de == "ubuntu" ]; then
-			gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
-		elif [ $de == "gnome" ]; then
-			gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
-		elif [ $de == "plasma" ]; then
-			qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///$picdir/$var")}'
-		elif [ $de == "xfce" ]; then
-			xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -n -t string -s "$picdir/$var"
-		elif [ $de == "mate" ]; then
-			dconf write /org/mate/desktop/background/picture-filename "$picdir/$var"
-		elif [ $de == "lxde" ]; then
-			pcmanfm --set-wallpaper="$picdir/$var"
-		fi
+	curl 2>&1 -o $var `curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1`
+	md5sum_1=`md5sum $var | awk '{print $1}'`
+	if [ $de == "ubuntu" ]; then
+		gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+	elif [ $de == "gnome" ]; then
+		gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+	elif [ $de == "plasma" ]; then
+		qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///$picdir/$var")}'
+	elif [ $de == "xfce" ]; then
+		xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -n -t string -s "$picdir/$var"
+	elif [ $de == "mate" ]; then
+		dconf write /org/mate/desktop/background/picture-filename "$picdir/$var"
+	elif [ $de == "lxde" ]; then
+		pcmanfm --set-wallpaper="$picdir/$var"
 	fi
-sleep 1h
+	
+	sleep 30m
+	
+	curl 2>&1 -o temp.png `curl 2>&1 /dev/null https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1`
+	md5sum_2=`md5sum temp.png | awk '{print $1}'`
+	if [ "$md5sum_1" == "$md5sum_2" ]; then
+		echo \n\n\n"No new abstracts."
+		mv temp.png $var
+	fi
 done
