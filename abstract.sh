8da66ab5bf5528d9161bf2b3b319bb3fe9a57271
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-05 06:32:33 +0100

    corrections

--- abstract.sh
+++ abstract.sh
@@ -14,6 +14,23 @@ de="xfce"
 #de="cinnamon"
 #-----------------
 
+setWallpaper() {
+	if [ $de == "ubuntu" ]; then
+		gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+	elif [ $de == "gnome" ]; then
+		gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
+	elif [ $de == "plasma" ]; then
+		qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///$picdir/$var")}'
+	elif [ $de == "xfce" ]; then
+		xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -n -t string -s "$picdir/$var"
+	elif [ $de == "mate" ]; then
+		dconf write /org/mate/desktop/background/picture-filename "$picdir/$var"
+	elif [ $de == "lxde" ]; then
+		pcmanfm --set-wallpaper="$picdir/$var"
+	elif [ $de == "cinnamon" ]; then
+		gsettings set org.cinnamon.desktop.background picture-uri  "file:///$picdir/$var"
+	fi
+}
 
 cd $picdir
 while [ 1 ]; do
@@ -21,23 +38,7 @@ while [ 1 ]; do
 	curl -o $var `curl https://vlepy.github.io/feed.xml | grep abstract | grep -v scaled_ | grep png | awk -F ";" '{print $3}' | awk -F "&" '{print $1}' | tail -n 1`
 	md5sum_1=`md5sum $var | awk '{print $1}'`
 	
-	setWallpaper() {
-		if [ $de == "ubuntu" ]; then
-			gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
-		elif [ $de == "gnome" ]; then
-			gsettings set org.gnome.desktop.background picture-uri file:///$picdir/$var
-		elif [ $de == "plasma" ]; then
-			qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///$picdir/$var")}'
-		elif [ $de == "xfce" ]; then
-			xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -n -t string -s "$picdir/$var"
-		elif [ $de == "mate" ]; then
-			dconf write /org/mate/desktop/background/picture-filename "$picdir/$var"
-		elif [ $de == "lxde" ]; then
-			pcmanfm --set-wallpaper="$picdir/$var"
-		elif [ $de == "cinnamon" ]; then
-			gsettings set org.cinnamon.desktop.background picture-uri  "file:///$picdir/$var"
-		fi
-	}
+
 	
 	setWallpaper
 	sleep 30m
