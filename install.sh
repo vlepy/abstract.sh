79a4866ace4c39a344bc073ec4fd4a525b2094e1
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-06 02:13:24 +0100

    corrections

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cp ./abstract.desktop /home/wsky/.config/autostart/
 cp ./abstract.desktop /home/wsky/.local/share/applications/
 chown $USER:$USER /home/wsky/.config/autostart/abstract.desktop 
 chown $USER:$USER /home/wsky/.local/share/applications/abstract.desktop
-cp ./nova.png /usr/share/icons
+cp ./abstract.svg /usr/share/icons
 echo "abstract.sh installed."
