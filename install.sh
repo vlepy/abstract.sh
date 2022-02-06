b33a2e9e6a2eae4c7db72e77a254ba49ae12c02f
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-06 02:09:14 +0100

    corrections

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cp ./abstract.sh /usr/local/bin
 cp ./ksetwallpaper.py /usr/local/bin
+cp ./abstract.desktop /home/wsky/.config/autostart/
+cp ./abstract.desktop /home/wsky/.local/share/applications/
+chown $USER:$USER /home/wsky/.config/autostart/abstract.desktop 
+chown $USER:$USER /home/wsky/.local/share/applications/abstract.desktop
+cp ./nova.png /usr/share/icons
 echo "abstract.sh installed."
