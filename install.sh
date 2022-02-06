fa80ffe134e1cf29574121d0c49e196d24bcf12c
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-06 07:10:17 +0100

    corrections

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#set your home directory path
+#set your home directory path and specify your username
 homedir=/home/wsky
-
+username=wsky
 cp ./abstract.sh /usr/local/bin
 cp ./ksetwallpaper.py /usr/local/bin
 cp ./abstract.desktop $homedir/.config/autostart/
 cp ./abstract.desktop $homedir/.local/share/applications/
-chown $USER:$USER $homedir/.config/autostart/abstract.desktop 
-chown $USER:$USER $homedir/.local/share/applications/abstract.desktop
+chown $username:$username $homedir/.config/autostart/abstract.desktop 
+chown $username:$username $homedir/.local/share/applications/abstract.desktop
 cp ./abstract.svg /usr/share/icons
 echo "abstract.sh installed."
