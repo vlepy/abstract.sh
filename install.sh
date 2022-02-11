83c80e614311b395fdcbba73ca53b6c2763f179c
Mirosław Leśniak <mir.lesniak@gmail.com>
2022-02-11 20:45:52 +0100

    corrections

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ username=wsky
 cp ./abstract.sh /usr/local/bin
 cp ./ksetwallpaper.py /usr/local/bin
 cp ./abstract.desktop $homedir/.config/autostart/
-cp ./abstract.desktop $homedir/.local/share/applications/
 chown $username:$username $homedir/.config/autostart/abstract.desktop 
 cp ./abstract.svg /usr/share/icons
 echo "abstract.sh installed."
+exec abstract.sh
